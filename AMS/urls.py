14a7896a7614ef230a1809e1b3b95a4fde020996
Shubham Kumar <shubhammisra800@gmail.com>
2025-10-03 00:14:41 +0530

    update more data

--- AMS/urls.py
+++ AMS/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from .views import *
 app_name='AMS'
 urlpatterns = [
     path('user/save',userSave,name='usersave'),
     path('user/list',userList,name='userlist'),
-    path('user/manipulate/<str:id>/',userApi,name='userapi')
+    path('user/manipulate/<str:id>/',userApi,name='userapi'),
+    path('course/',course,name='course'),
+    path('courseapi/<str:id>/',courseApi,name='courseapi'),
+    
 ]
--- AMS/views.py
+++ AMS/views.py
@@ -41,3 +41,37 @@ def userApi(request,id):
         return Response(status=status.HTTP_200_OK)
     
 
+@api_view(['POST','GET'])
+def course(request):
+    serilize=CourseSerializer(data=request.data)
+    if  request.method=='POST':
+       if serilize.is_valid():
+            serilize.save()
+            return Response(serilize.data,status=status.HTTP_201_CREATED)
+       else:
+           return Response(serilize.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method=='GET':
+        course=Course.objects.all()
+        serilize=CourseSerializer(course,many=True)
+        return Response(serilize.data,status=status.HTTP_200_OK)
+    
+    else:
+        return Response(serilize.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT','DELETE','GET'])
+def courseApi(request,id):
+    try:
+        course=Course.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_451_UNAVAILABLE_FOR_LEGAL_REASONS)
+    if request.method=='PUT':
+        serilize=UserSerilizer(instance=course,data=request.data)
+        if serilize.is_valid():
+            serilize.save()
+            return Response(serilize.data,status=status.HTTP_202_ACCEPTED)
+        return Response(serilize.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method=='DELETE':
+        course.delete()
+        return Response(status=status.HTTP_200_OK)
+          
